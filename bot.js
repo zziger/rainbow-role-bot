b0b77a6997917580c5b7ac8afdfc7538667b446c
Dzhemesyuk Artem <zziger07@gmail.com>
2018-06-06 14:57:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,4 +13,8 @@ async function color () {
 client.on('ready', () => {
     color();
 });
+client.on('guildCreate', (guild) => {
+    let channels = guild.channels.filter(channel => channel.type === 'text' && channel.permissionsFor(guild.members.get(client.user.id)).has('SEND_MESSAGES'));
+    if (channels.size > 0) channels.first().send('Вы пригласили бота **Rainbow Role**.\nДля его корректного функционирования у вас на сервере должна быть роль `Rainbow`, роль бота должна иметь право `управление ролями`, и быть выше роли `Rainbow`.\nЕсли у вас возникли какие-то трудности - обратитесь к <@421030089732653057> (`zziger#8040`)');
+});
 client.login(process.env.TOKEN);
